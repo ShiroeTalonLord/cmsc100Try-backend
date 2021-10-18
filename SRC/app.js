d8d76a99a11efdc30b15db60716d261a78a73777
ShiroeTalonLord <rsmanalo1@up.edu.ph>
2021-10-18 06:54:13 +0000

    feat:bastayun

--- SRC/app.js
+++ SRC/app.js
@@ -1,6 +1,9 @@
 // Require the framework and instantiate it
 import fastify from 'fastify';
-import { hehelols } from './services/base/home.js'
+import FastifyOpenApiGlue from 'fastify-openapi-glue';
+import fastifySwagger from 'fastify-swagger';
+import { specification } from './specifications/index.js';
+//import { hehelols } from './services/base/home.js'
 import { WARAFU } from './services/base/LOLOLOL/WARAFU.js'
 /**
  * This function starts the server
@@ -14,6 +17,12 @@ export async function server (options = { logger: true}){
   const app = fastify(options);
   app.get('/', WARAFU);
   app.post('/', WARAFU);
-  app.get('/Micky', hehelols);
+  //app.get('/Micky', hehelols);
+  const openApiOptions = {
+    specification
+  };
+ 
+ 
+ 
   return app;
 }
